a2d8a6e374102af7fd784e7a324c750e6d32b034
Shreyas <Shreyas@192.168.2.16>
2021-05-10 12:50:10 -0400

    added HumanPlayer class

---
+++ Software Engineering/src/may17/HumanPlayer.java
@@ -0,0 +1,17 @@
+package may17;
+/**
+ * A class to create a human player object, that would be used to represent the user and perform certain moves.
+ * @author Shreyas
+ * @version 1.0, May 10, 2021
+ */
+public class HumanPlayer {
+	private String symbol;
+	private String name;
+	
+	
+	public HumanPlayer(String name, String symbol) {
+		this.name = name;
+		this.symbol = symbol;
+	}
+
+}
