5087dee3e88fffae3336437bc2907a46015377b0
ardav <ardav@192.168.2.240>
2021-05-12 12:30:51 -0400

    mainMenu to MainMenu

--- Software Engineering/src/may17/MainMenu.java
+++ Software Engineering/src/may17/MainMenu.java
@@ -10,7 +10,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class mainMenu {
+public class MainMenu {
 
 	private JFrame frame;
 
