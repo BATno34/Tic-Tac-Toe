232b07671dbfe0c27f96a0d836f64f003bcd9975
Shreyas <Shreyas@192.168.2.16>
2021-05-17 13:08:35 -0400

    javadocs for HumanPlayer

--- Software Engineering/src/may17/HumanPlayer.java
+++ Software Engineering/src/may17/HumanPlayer.java
@@ -11,28 +11,54 @@ public class HumanPlayer {
 	private String symbol; // the symbol that will be used to represent the player on the game board, typically X or O
 	private String name;
 	
-	
+	/**
+	 * Constructor to create a HumanPlayer object
+	 * @param name, the player's name
+	 * @param symbol, the symbol that will represent the player
+	 */
 	public HumanPlayer(String name, String symbol) {
 		this.name = name;
 		this.symbol = symbol;
 	}
 	
+	/**
+	 * Gets the name of the player
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Gets the symbol of the player
+	 * @return the symbol
+	 */
 	public String getSymbol() {
 		return this.symbol;
 	}
 	
+	/**
+	 * Changes the name of the player to the specified argument
+	 * @param name, the name for the player's name to be changed to
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Changes the player's symbol
+	 * @param symbol, the new symbol for the player's symbol to be changed to
+	 */
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
 	
+	/**
+	 * Makes a move for the human player based on the move chosen by the user
+	 * @param spacesAvailable, an ArrayList containing the spaces on the board that are still available
+	 * @param humanMoves, an ArrayList containing all the moves made by the human player
+	 * @param chosenMove, the square on the grid that the player has chosen
+	 */
 	public void makeMove(ArrayList<Integer> spacesAvailable, ArrayList<Integer> humanMoves, int chosenMove) {
 		int moveIndex = spacesAvailable.indexOf(chosenMove);
 		spacesAvailable.remove(moveIndex);
