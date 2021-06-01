192c17a5a56f175124835632cd2e2bbc2d6788cc
ardav <ardav@DESKTOP-OBJL7E1>
2021-06-01 15:47:28 -0400

    test

--- Software Engineering/src/may17/MainMenu.java
+++ Software Engineering/src/may17/MainMenu.java
@@ -16,6 +16,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class MainMenu {
 
@@ -51,12 +53,9 @@ public class MainMenu {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1000, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		
 		JLabel lblTitle = new JLabel("Tic Tac Toe");
 		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblTitle.setBounds(414, 46, 134, 42);
-		frame.getContentPane().add(lblTitle);
 		
 		JButton btnStartButton = new JButton("Start Game");
 		btnStartButton.addActionListener(new ActionListener() {
@@ -66,8 +65,6 @@ public class MainMenu {
 				instFrame.setVisible(true);
 			}
 		});
-		btnStartButton.setBounds(380, 173, 201, 65);
-		frame.getContentPane().add(btnStartButton);
 		
 		JButton btnChange = new JButton("Change Difficulty");
 		btnChange.addActionListener(new ActionListener() {
@@ -78,8 +75,6 @@ public class MainMenu {
 
 			}
 		});
-		btnChange.setBounds(380, 285, 201, 65);
-		frame.getContentPane().add(btnChange);
 		
 		JButton btnQuitGame = new JButton("Quit Game");
 		btnQuitGame.addActionListener(new ActionListener() {
@@ -88,7 +83,39 @@ public class MainMenu {
 				frame.dispose();     //closes the frame
 			}
 		});
-		btnQuitGame.setBounds(380, 417, 201, 65);
-		frame.getContentPane().add(btnQuitGame);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(414)
+					.addComponent(lblTitle, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+					.addGap(440))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(380)
+					.addComponent(btnStartButton, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+					.addGap(407))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(380)
+					.addComponent(btnChange, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+					.addGap(407))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(380)
+					.addComponent(btnQuitGame, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+					.addGap(407))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(46)
+					.addComponent(lblTitle, GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
+					.addGap(85)
+					.addComponent(btnStartButton, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(47)
+					.addComponent(btnChange, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(67)
+					.addComponent(btnQuitGame, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(83))
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 }
